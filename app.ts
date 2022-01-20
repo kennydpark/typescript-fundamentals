6c6b74b023fa0dd4a4507e5210301fc91a46392e
Kenny Park <kennyparc@gmail.com>
2022-01-20 04:03:52 +0000

    working with function types of void and undefined, and unknown variable types.

--- app.ts
+++ app.ts
@@ -1,30 +1,15 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+let userInput: unknown;
+let userName: string;
 
-const combine = (
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-  ) => {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result; // or parseFloat(result);
-  // } else {
-  //   return result.toString();
-  // }
+userInput = 5;
+userInput = 'Kenny';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function generateError(message: string, code: number): never { // intended to never run
+  throw { message: message, errorCode: code };
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Kenny', 'Park', 'as-text');
-console.log(combinedNames);
+const result = generateError('An error occurred!', 500);
+// console.log(result)
