bfdd976be7d4560c5002c5a03da0e978c07cf9c7
Kenny Park <kennyparc@gmail.com>
2022-02-08 06:00:23 +0000

    interfaces with classes and function types.

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,139 +1,50 @@
-abstract class Department {
-  // 'statc' - separated from instances and not accessible via 'this'
-  static fiscalYear = 2022;
-  // private readonly id: string;
-  // private name: string;
-  // private employees: string[] = [];
-  protected employees: string[] = [];
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
-  constructor(protected readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = n;
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string; // optional param type denoted by '?'
+}
+
+interface Greetable extends Named {
+  readonly name?: string; // cannot use private/public on interfaces
+
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable { // classes can inherit only one, but interfaces can multiple
+  name?: string;
+  age = 29;
+
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  abstract describe(this: Department): void;
-    // console.log(`Department: ${this.id}: ${this.name}`)
-
-  addEmployee(employee: string) {
-    this.employees.push(employee)
-  }
-
-  printEmployeeInformation() {
-    console.log('number of employees: ', this.employees.length);
-    console.log(this.employees);
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
-class ITDepartment extends Department {
-  admins: string[];
-  constructor(id: string, admins: string[]) {
-    super(id, 'IT');
-    this.admins = admins;
-  }
+let user1: Greetable;
 
-  describe() {
-    console.log('IT Department - ID: ' + this.id);
-  }
-}
+user1 = new Person('Kenny');
+// user1.name = 'Lenny'; // does not work because readonly line 2
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
+user1.greet('Hi there - I am');
+console.log(user1);
 
-// getter must return something
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error('No report found.');
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error('Please pass in a valid value!');
-    }
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment('d2', []);
-    return this.instance;
-  }
-
-  describe() {
-    console.log('Accounting Department - ID: ' + this.id);
-  }
-
-// overriding methods/properties of base class
-  addEmployee(name: string) {
-    if (name === 'Kenny') {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports)
-  }
-}
-
-const department = new Department('dx', 'Department');
-console.log(department)
-department.describe();
-
-// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
-// accountingCopy.describe();
-
-department.addEmployee('Employee1');
-department.addEmployee('Employee2');
-department.printEmployeeInformation();
-
-
-
-// - alternative way, but not recommended for uniformity in method
-// - and doesn't work when 'private' keyword is added in the class
-// - otherwise, 'public' enables the access, which is by default
-// accounting.employees[2] = 'Lenny';
-// accounting.printEmployeeInformation();
-
-const employee1 = Department.createEmployee('Kenny');
-console.log(employee1, Department.fiscalYear);
-
-const it = new ITDepartment('d1', ['Kenny']);
-
-
-
-it.addEmployee('Kenny');
-it.addEmployee('Jenny');
-it.printEmployeeInformation();
-console.log(it);
-
-// const accounting = new AccountingDepartment('d2', []);
-const accounting = AccountingDepartment.getInstance();
-
-accounting.mostRecentReport = 'Year End Report';
-accounting.addReport('Something went wrong...');
-accounting.printReports();
-accounting.addEmployee('Kenny');
-accounting.addEmployee('Benny');
-accounting.printEmployeeInformation();
-console.log(accounting);
-console.log(accounting.mostRecentReport);
-accounting.describe();
+// interfaces only exist in dev in TS, not in JS
---
+++ cl-interfaces-01-starting-setup/src/classes.ts
@@ -0,0 +1,139 @@
+abstract class Department {
+  // 'statc' - separated from instances and not accessible via 'this'
+  static fiscalYear = 2022;
+  // private readonly id: string;
+  // private name: string;
+  // private employees: string[] = [];
+  protected employees: string[] = [];
+
+  constructor(protected readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  abstract describe(this: Department): void;
+    // console.log(`Department: ${this.id}: ${this.name}`)
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInformation() {
+    console.log('number of employees: ', this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+
+  describe() {
+    console.log('IT Department - ID: ' + this.id);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+// getter must return something
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!');
+    }
+    this.addReport(value);
+  }
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
+  }
+
+  describe() {
+    console.log('Accounting Department - ID: ' + this.id);
+  }
+
+// overriding methods/properties of base class
+  addEmployee(name: string) {
+    if (name === 'Kenny') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports)
+  }
+}
+
+// const department = new Department('dx', 'Department');
+// console.log(department)
+// department.describe();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
+// accountingCopy.describe();
+
+// department.addEmployee('Employee1');
+// department.addEmployee('Employee2');
+// department.printEmployeeInformation();
+
+
+
+// - alternative way, but not recommended for uniformity in method
+// - and doesn't work when 'private' keyword is added in the class
+// - otherwise, 'public' enables the access, which is by default
+// accounting.employees[2] = 'Lenny';
+// accounting.printEmployeeInformation();
+
+const employee1 = Department.createEmployee('Kenny');
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment('d1', ['Kenny']);
+
+
+
+it.addEmployee('Kenny');
+it.addEmployee('Jenny');
+it.printEmployeeInformation();
+console.log(it);
+
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+
+accounting.mostRecentReport = 'Year End Report';
+accounting.addReport('Something went wrong...');
+accounting.printReports();
+accounting.addEmployee('Kenny');
+accounting.addEmployee('Benny');
+accounting.printEmployeeInformation();
+console.log(accounting);
+console.log(accounting.mostRecentReport);
+accounting.describe();
